0aa86cfce935c02897cd2f42279c77d695dbeaec
sookiemonster <sookiemonster216@gmail.com>
2023-04-26 23:40:46 -0400

    Add basic automatic article loading

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import os, json
 
 MASTER = os.path.join("articles", "timeline.json")
+ARTICLE_DIR = "articles"
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='')
@@ -26,7 +27,20 @@ def resume():
     with open(MASTER, "r") as f:
         timeline_data = json.loads(f.read())
 
-    return render_template("content.j2", timeline=timeline_data)
+    text_data = {}
+    for section in timeline_data:
+        for event in timeline_data.get(section).get('event_list'):
+            print(event)
+            text_data[event.get('title')] = {}
+            with open(os.path.join(ARTICLE_DIR, event.get('content_file')), "r") as f:
+                text_data[event.get('title')]['title'] = event.get('title')
+                text_data[event.get('title')]['text'] = f.read()
+                text_data[event.get('title')]['anchor'] = event.get('anchor')
+
+    print(text_data)
+    return render_template("content.j2", 
+                           timeline=timeline_data,
+                           content=text_data)
 
 if __name__ == "__main__":
     app.debug = True
