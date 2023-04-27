01b4d8ef22d5f1854ebc1ad3797af98805e3aae4
sookiemonster <sookiemonster216@gmail.com>
2023-04-26 23:04:14 -0400

    Add jinja automated timeline

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os, json
 
-DATA_FILE = ""
+MASTER = os.path.join("articles", "timeline.json")
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='')
@@ -22,11 +22,11 @@ app = create_app()
 
 @app.route("/", methods=['GET', 'POST'])
 def resume():
-    data = {}
-   #  with open(DATA_FILE, "r") as f:
-   #      data = json.loads(f.read())
-   
-    return render_template("content.j2")
+    timeline_data = {}
+    with open(MASTER, "r") as f:
+        timeline_data = json.loads(f.read())
+
+    return render_template("content.j2", timeline=timeline_data)
 
 if __name__ == "__main__":
     app.debug = True
--- js/timeline.js
+++ js/timeline.js
@@ -7,7 +7,7 @@ let sectionIdPrefix = "timeline-sec-";
 /**
  * Remove '.active' from all timeline elements
  */
-let strip_active = () => {
+let clear_active = () => {
    for (i = 0; i < timeline_events.length; i++) {
       timeline_events[i].classList.remove('active');
    }  
@@ -24,7 +24,7 @@ let init_listeners = () => {
    for (i = 0; i < timeline_events.length; i++) {
       timeline_events[i].addEventListener('click', (e) => {
          // console.log(e.target);
-         strip_active();
+         clear_active();
          // Highlight event 
          highlightElement(e.currentTarget);
 
@@ -43,10 +43,38 @@ let init_listeners = () => {
    for (i = 0; i < timeline_sections.length; i++) {
       timeline_sections[i].addEventListener('click', (e) => {
          // console.log(e.target);
-         strip_active();
+         clear_active();
          e.currentTarget.classList.add('active');
       });
    }
 }
 
 init_listeners();
+highlightElement(timeline_sections[0]);
+
+// Highlight Scrolling
+
+// let options = {
+//    rootMargin: '0px',
+//    threshold: 0.2
+// }
+ 
+
+// let callback = (entries, observer) => {
+//    entries.forEach(entry => {
+//       if (entry.isIntersecting){
+//          clear_active();
+//          console.log(entry.target);
+//          let topic = entry.target.dataset.topic;
+//          let navItem = document.querySelector(`a[href="#${topic}"]`).parentElement;
+//          // console.log(navItem);
+//          updateCurrent(navItem);
+//       }
+//    });
+// };
+
+// let observer = new IntersectionObserver(callback, options);
+
+// for (let i = 0; i < topics.length; i++) {
+//    observer.observe(topics[i]);
+// };
